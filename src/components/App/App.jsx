5bce33254d928d65271b0747a4b3d23c4a2d1f1b
Serhii Yatsenko <serhii2111@yahoo.com>
2024-03-02 23:25:08 +0200

    app_useEffect

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,19 +1,28 @@
+import { useEffect } from "react";
+import "../../../node_modules/modern-normalize/modern-normalize.css";
+// import axios from "axios";
 // import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import css from './App.module.css';
+import axios from "axios";
 
 export default function App() {
   
 
+  useEffect(() => {
+    async function fetchArticles() {
+      const response = await axios.get(
+        "<https://hn.algolia.com/api/v1/search?query=react>"
+      );
+      console.log(response);
+    }
+    fetchArticles();
+  }, []);
+
   return (
     <div className={css.container}>
-      <a href="https://vitejs.dev" target="_blank">
-        {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
-      </a>
-      <a href="https://react.dev" target="_blank">
-        {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
-      </a>
+     <h1>Latest articles</h1>
     </div>
   );
 }
