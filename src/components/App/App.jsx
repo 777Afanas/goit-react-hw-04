8f6245cf54f5714f4d1e81f79b2b68cf9913b16a
Serhii Yatsenko <serhii2111@yahoo.com>
2024-03-02 23:10:09 +0200

    structure

---
+++ src/components/App/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import css from './App.module.css';
+
+export default function App() {
+  
+
+  return (
+    <div className={css.container}>
+      <a href="https://vitejs.dev" target="_blank">
+        {/* <img src={viteLogo} className="logo" alt="Vite logo" /> */}
+      </a>
+      <a href="https://react.dev" target="_blank">
+        {/* <img src={reactLogo} className="logo react" alt="React logo" /> */}
+      </a>
+    </div>
+  );
+}
+
+
